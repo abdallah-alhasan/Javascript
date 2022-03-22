b41e5b9442463f256cf305a7bdf10a5525644764
abdallah-alhasan <eng.abdallahalhasan@gmail.com>
2022-03-22 21:43:44 +0300

    JS

--- JS-Tasks/FunctionTasks/Task2/main.js
+++ JS-Tasks/FunctionTasks/Task2/main.js
@@ -1,199 +1,93 @@
-/*
- * Exercise 1:
- *
- * Make an array of numbers that are doubles of the first array
- *
- * Test Case:
- *
- * console.log(doubleNumbers([2, 5, 100]));
- *
- * Result:
- * [4, 10, 200]
- */
-function doubleNumbers(double) {
-  for (let i = 0; i < double.length; i++) {
-    double[i] = double[i] * 2;
-  }
-  return double;
+/* 1. Write a JavaScript program that accept two integers and
+ * display the larger. Go to the editor */
+/******* Start Your Code *********/
+let num1 = window.prompt("Enter a number: ");
+let num2 = window.prompt("Enter a number: ");
+if(num1>num2){
+    alert(num1);
+}else{
+    alert(num2);
 }
-console.log(doubleNumbers([2, 5, 100]));
-/*
- * Exercise 2 :
- *
- * Take an array of numbers and make them strings
- *
- * Test Case:
- *
- * console.log(stringItUp([2, 5, 100]));
- *
- * Result:
- * ["2", "5", "100"]
- */
+ /******* End Your Code ********* */
 
-function stringItUp(string) {
-  newArr = [];
-  for (i = 0; i < string.length; i++) {
-    newArr[i] = string[i].toString();
-  }
-
-  return newArr;
+/* 2. Write a JavaScript conditional statement to find the sign
+ of product of three numbers. Display an alert box with the 
+ specified sign. Go to the editor 
+Sample numbers : 3, -7, 2 
+Output : The sign is - */
+/******* Start Your Code *********/
+let num3 = window.prompt("Product  Enter a number:4 ");
+let num4 = window.prompt(" Product Enter a number: ");
+let num5 = window.prompt("Product Enter a number: ");
+product = num3 * num4 * num5;
+if(product < 0){
+ alert("the sign is -"); 
+}else{
+ alert("the sign is (“+”)");
 }
-console.log(stringItUp([2, 5, 100]));
-/*
- * Exercise 3:
- *
- * Capitalize each of an array of names
- *
- * Test Case:
- *
- * console.log(capitalizeNames(["john", "JACOB", "jinGleHeimer", "schmidt"]));
- *
- * Result:
- *  ["John", "Jacob", "Jingleheimer", "Schmidt"]
- */
 
-function capitalizeNames(arr) {
-  for (var i = 0; i < arr.length; i++) {
-    arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].substr(1);
-  }
-  return arr;
+ /******* End Your Code ********* */
+
+/* 3. Write a JavaScript conditional statement to sort three numbers. Display an alert box to show the result. Go to the editor 
+Sample numbers : 0, -1, 4 
+Output : 4, 0, -1 */
+/******* Start Your Code *********/
+let num6 = window.prompt("Sort Enter a number: ");
+let num7 = window.prompt("Sort Enter a number: ");
+let num8 = window.prompt("Sort Enter a number: ");
+if(num6 > num7 && num6 > num8){
+	if(num7 > num8){
+	alert(num6 + " " + num7 +" " + num8);	
+}else{
 }
-document.write(
-  `${capitalizeNames(["john", "JACOB", "jinGleHeimer", "schmidt"])} <br>`
-);
-
-/*
- * Exercise 4:
- *
- * Make an array of the names in h1s, and the ages in h2s
- *
- * Test Case:
- *
- * console.log(readyToPutInTheDOM([
- *   {
- *       name: "Angelina Jolie",
- *       age: 80
- *   },
- *   {
- *       name: "Eric Jones",
- *       age: 2
- *   },
- *   {
- *       name: "Paris Hilton",
- *       age: 5
- *   },
- *   {
- *       name: "Kayne West",
- *       age: 16
- *   },
- *   {
- *       name: "Bob Ziroll",
- *       age: 100
- *   }
- * ]));
- *
- * Result:
- *
- * ["<h1>Angelina Jolie</h1><h2>80</h2>",
- * "<h1>Eric Jones</h1><h2>2</h2>",
- * "<h1>Paris Hilton</h1><h2>5</h2>",
- * "<h1>Kayne West</h1><h2>16</h2>",
- * "<h1>Bob Ziroll</h1><h2>100</h2>"]
- */
-
-function readyToPutInTheDOM(arr) {
-  arr.forEach((key) => {
-    document.write("<h1>" + key.name + "</h1>");
-    document.write("<h2>" + key.age + "</h2>");
-  });
-  return arr;
+alert(num6 + " " + num8 +" " + num7);	
+}else if(num7 > num6 && num7 > num8){
+if(num6 > num8){
+	alert(num7 + " " + num6 +" " + num8);	
+}else{
+    alert(num7 + " " + num8 +" " + num6);
 }
-readyToPutInTheDOM([
-  {
-    name: "Angelina Jolie",
-    age: 80,
-  },
-  {
-    name: "Eric Jones",
-    age: 2,
-  },
-  {
-    name: "Paris Hilton",
-    age: 5,
-  },
-  {
-    name: "Kayne West",
-    age: 16,
-  },
-  {
-    name: "Bob Ziroll",
-    age: 100,
-  },
-]);
-
-/*
- * Exercise 5:
- * Write a function called doubleValues which accepts an array and returns a new array with all the values in the array passed to the function doubled
- *
- * Test Cases:
- *   Test Case 1:  doubleValues([1,2,3])
- *   Test Case 2:  doubleValues([1,-2,-3])
- *
- * Result:
- * Test Case 1: [2,4,6]
- * Test Case 2: [2,-4,-6]
- */
-
-function doubleValues(double) {
-    for (let i = 0; i < double.length; i++) {
-      double[i] = double[i] * 2;
-    }
-    return double;
-  }
-  document.write(`${doubleNumbers([1,-2,-3])} <br>`);
-
-/*
- * Exercise 6:
-epts an array and returns a new array with each value multiplied by the index it is currently at in the array. * Write a function called valTimesIndex which acc
- *
- *
- * Test Cases :
- *   Test Case 1:  valTimesIndex([1,2,3])
- *   Test Case 2:  valTimesIndex([1,-2,-3])
- *
- * Result
- * Test Case 1: [0,2,6]
- * Test Case 2: [0,-2,-6]
- */
-
-function valTimesIndex(index) {
-  for (let i = 0; i < index.length; i++) {
-    index[i] = index[i] * i;
-  }
-
-  return index;
+}else{
+if(num6 > num7){
+    alert(num8 + " " + num6 +" " + num7);	
+}else{
+    alert(num8 + " " + num7 +" " + num6);	
 }
-document.write(`${valTimesIndex([1,-2,-3])} <br>`)
-/*
- * Exercise 7:
- * Write a function called extractKey which accepts an array of objects and some key and returns a new array with the value
- * of that key in each object.
- *
- * Test Case:
- * extractKey([{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}], 'name')
- *
- * Result:
- * ['Elie', 'Tim', 'Matt', 'Colt']
- */
-
-/*
- * Exercise 8:
- * Write a function called extractFullName which accepts an array of objects and returns a new array with the value of the key with a name of "first" and the value of a key with the name of  "last" in each object, concatenated together with a space.
- *
- * Examples:
- * extractFullName([{first: 'Elie', last:"Schoppik"}, {first: 'Tim', last:"Garcia"}, {first: 'Matt', last:"Lane"}, {first: 'Colt', last:"Steele"}]) // ['Elie Schoppik', 'Tim Garcia', 'Matt Lane', 'Colt Steele']
- */
-
-function extractFullName() {
-  // your code here
 }
+ /******* End Your Code ********* */
+
+
+/*4. Write a JavaScript conditional statement to find the largest of five numbers. 
+Display an alert box to show the result. Go to the editor 
+Sample numbers : -5, -2, -6, 0, -1 
+Output : 0 */
+
+/******* Start Your Code *********/
+let num9 = window.prompt("Lets see which is a bigger number,Enter a number: ");
+let num10 = window.prompt("Lets see which is a bigger number,Enter a number:");
+let num11 = window.prompt("Lets see which is a bigger number,Enter a number:");
+  if(num9 > num10 || num9 > num11 ||  num9 > num4 ||  num9 > num5){
+	alert("number"  +  num9 +  "is the biggest")
+}else   if(num10 > num9 || num10 > num11 ||  num10 > num4 ||  num10 > num5){
+    alert("number"  +  num10 +  "is the biggest")
+}else   if(num11 > num10 || num11 > num9 ||  num11 > num4 ||  num11 > num5){
+    alert("number"  +  num11 +  "is the biggest")
+} else   if(num4 > num10 || num4 > num11 ||  num4> num9||  num4 > num5){
+    alert("number"  +  num4 +  "is the biggest")
+}else   if(num5 > num10 || num5 > num11 ||  num5 > num4 ||  num5 > num9){
+    alert("number"  +  num5 +  "is the biggest")
+}
+ /******* End Your Code ********* */
+
+ /* 5.Write the if statement to display "Hello World" if x is greater than y, otherwise alert "Goodbye".*/
+/******* Start Your Code *********/
+let x = window.prompt("Enter a number: ");
+let y = window.prompt("Enter a number: ");
+  if(x > y){
+alert("hello world");
+}else{
+alert("Goodbye")
+}
+ /******* End Your Code ********* */
+
+
