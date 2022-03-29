0830dc110be0ae22d72e9c9c49655417ce156bd4
abdallah-alhasan <eng.abdallahalhasan@gmail.com>
2022-03-29 20:31:42 +0300

    JS

--- JS-Tasks/29-03-2022/Task1/main.js
+++ JS-Tasks/29-03-2022/Task1/main.js
@@ -1,5 +1,6 @@
 let select =document.getElementById("char")
 let option = document.getElementsByTagName("option")
+let myDiv =document.createElement("div")
 async function getData() {
   const response =await fetch("https://www.breakingbadapi.com/api/characters");
   const data =await response.json();
@@ -7,7 +8,6 @@ async function getData() {
     select.innerHTML += `<option value = "${i}"> ${data[i].name} </option>`
   }
     select.onchange = function nickName(){
-          let myDiv =document.createElement("div")
           myDiv.classList.add("nickName")
           myDiv.innerHTML = `Nickname: ${data[this.value].nickname}`
           select.after(myDiv) 
